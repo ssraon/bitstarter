3accefba8130a752dad7fba98417e9b5527c9316
Siva Nagabhyru <ssrao_n@yahoo.com>
2013-07-10 04:36:16 +0000

    modified 1

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World4!');
+
+var fs = require('fs');
+var buf=require('buf');
+var buf=new Buffer(fs.readFileSync("index.html");
+
+  response.send(buf.toString(buf);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+
   console.log("Listening on " + port);
 });
